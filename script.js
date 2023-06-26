be1ac08c3c4a9759c3b7d16da72855a2fa587fbf
Sean <swakal367@gmail.com>
2023-06-26 10:56:48 +0800

    more back to top attempts

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-window.onscroll = function() {
-    showBackToTopBtn();
-};
+var backToTopBtn = document.querySelector('.back-to-top-btn');
 
-function showBackToTopBtn() {
-    var backToTopBtn = document.getElementById("back-to-top-btn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-}
-}
-    function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-}
-var backToTopBtn = document.getElementById("back-to-top-btn");
-backToTopBtn.addEventListener("click", backToTop);
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 10) {
+    backToTopBtn.classList.add('show-back-to-top-btn');
+  } else {
+    backToTopBtn.classList.remove('show-back-to-top-btn');
+  }
+});
+
+backToTopBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
