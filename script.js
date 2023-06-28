adb95f1c9c9b8a666443aa3f8b4bf4827088ea95
Sean <swakal367@gmail.com>
2023-06-28 14:18:12 +0800

    button finnaly work

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myBtn.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myBtn.style.display = "none";
   }
 }
 
