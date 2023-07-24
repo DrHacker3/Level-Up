7398d5cb254e2bc4f0322a00f281641931e4e0fe
Sean <swakal367@gmail.com>
2023-07-24 10:56:49 +0800

    added subscribe to newsletter

--- script.js
+++ script.js
@@ -17,3 +17,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// Subscribe to newsletter
+var modal = document.getElementById('id01');
