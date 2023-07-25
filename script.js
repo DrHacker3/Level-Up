ef44fb8c127490168c35a5acfd644ec23cc7ef25
Sean <swakal367@gmail.com>
2023-07-25 09:38:00 +0800

    Updated code and made more borders round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Get the button:
+// Button
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// show the button on scroll
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,10 +12,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// scroll back to top of page on click
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // safari functionality
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera funtionality
 }
 
 
